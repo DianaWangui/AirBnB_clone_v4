c3836d7f6966948378fb19b3b261ba677a6fc5ed
DANKAHIA <dankahia86@gmail.com>
2024-05-04 16:59:06 +0000

    0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -9,8 +9,7 @@ from os import environ
 from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+
 
 
 cached_id = uuid.uuid4()
